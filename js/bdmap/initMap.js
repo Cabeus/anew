7ed92608fb298b3030ec931cb19f76fa05dc9325
Cabeus <cabeus@qq.com>
2018-02-24 18:33:33 +0800

    1

--- js/bdmap/initMap.js
+++ js/bdmap/initMap.js
@@ -289,10 +289,12 @@ function initMap(selector, bmapConfig) {
     };
 
 
-    /**
-     *
-     */
+
+
     this.DrawingManager = function (type) {
+
+        //http://api.map.baidu.com/library/DrawingManager/1.4/docs/symbols/BMapLib.DrawingManager.html
+
         let MapTypeHtml = '<!-- 绘制工具操作 -->\n' +
             '<div class="drawing-manager">\n' +
             '<a class="map-opt-center"><i class="fa fa-dot-circle-o" aria-hidden="true"></i><span>中心</span></a>\n' +
@@ -304,17 +306,18 @@ function initMap(selector, bmapConfig) {
 
 
         let styleOptions = {
-            strokeColor:"red",    //边线颜色。
-            fillColor:"red",      //填充颜色。当参数为空时，圆形将没有填充效果。
+            strokeColor:"blue",    //边线颜色。
+            // fillColor:"red",      //填充颜色。当参数为空时，圆形将没有填充效果。
             strokeWeight: 3,       //边线的宽度，以像素为单位。
             strokeOpacity: 0.8,	   //边线透明度，取值范围0 - 1。
             fillOpacity: 0.6,      //填充的透明度，取值范围0 - 1。
-            strokeStyle: 'solid' //边线的样式，solid或dashed。
+            strokeStyle: 'dashed',//边线的样式，solid或dashed。
+            // enableEditing : true     // 编辑模式
         };
         //实例化鼠标绘制工具
         let drawingManager = new BMapLib.DrawingManager(map, {
             isOpen: false, //是否开启绘制模式
-            enableDrawingTool: true, //是否显示工具栏
+            enableDrawingTool: false, //是否显示工具栏
             drawingToolOptions: {
                 anchor: BMAP_ANCHOR_TOP_RIGHT, //位置
                 offset: new BMap.Size(5, 5), //偏离值
@@ -325,30 +328,53 @@ function initMap(selector, bmapConfig) {
             rectangleOptions: styleOptions //矩形的样式
         });
 
+        $('#' + selector + ' .map-opt-center').click(function () {
+            // 画点
+            specialPointMode = true;
+            drawingManager.open();
+            drawingManager.setDrawingMode(BMAP_DRAWING_MARKER);
+        });
+
+        $('#' + selector + ' .map-opt-point').click(function () {
+            // 画点
+            specialPointMode = false;
+            drawingManager.open();
+            drawingManager.setDrawingMode(BMAP_DRAWING_MARKER);
+        });
+
+        $('#' + selector + ' .map-opt-region').click(function () {
+            //画线
+            drawingManager.open();
+            drawingManager.setDrawingMode(BMAP_DRAWING_POLYLINE);
+        });
+        $('#' + selector + ' .map-opt-clear').click(function () {
+            // 清空
+            map.clearOverlays();
+            if(drawingManager) {
+                drawingManager.close();
+            }
+
+        });
+
+        // let overlays = [];
+        // let overlaycomplete = function(e){
+        //     overlays.push(e.overlay);
+        //     console.log(overlays);
+        // };
+        // //添加鼠标绘制工具监听事件，用于获取绘制结果
+        // drawingManager.addEventListener('overlaycomplete', overlaycomplete);
 
 
-        var overlays = [];
-        var overlaycomplete = function(e){
-            overlays.push(e.overlay);
-        };
-        //添加鼠标绘制工具监听事件，用于获取绘制结果
-        drawingManager.addEventListener('overlaycomplete', overlaycomplete);
+        //监听绘制点事件
+        drawingManager.addEventListener("markercomplete", function(e, overlay) {
+            console.log(overlay);
+        });
 
+        //监听绘制多边形事件
+        drawingManager.addEventListener("polylinecomplete", function(e, overlay) {
+            console.log(overlay);
+        });
 
-
-
-        // 画点
-        // specialPointMode = true;
-        // drawingManager.open();
-        // drawingManager.setDrawingMode(BMAP_DRAWING_MARKER);
-
-        // specialPointMode = false;
-        // drawingManager.open();
-        // drawingManager.setDrawingMode(BMAP_DRAWING_MARKER);
-
-
-        drawingManager.open();
-        drawingManager.setDrawingMode(BMAP_DRAWING_POLYLINE);
 
     }
 
